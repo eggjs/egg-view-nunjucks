bf4778a3e68ede4461fe9ec41ccf2dba9f561285
liuxuewei <xuewei.lxw@alibaba-inc.com>
2021-01-15 15:33:21 +0800

    添加clb hackRender 单侧

--- test/fixtures/example/app/router.js
+++ test/fixtures/example/app/router.js
@@ -1,13 +1,42 @@
 'use strict';
 const path = require('path');
 const fs = require('fs');
+
+function hackRender(engine) {
+  // const originRender = engine.render.bind(engine);
+  const originRenderString = engine.renderString.bind(engine);
+
+  // engine.render = function(name, context, callback) {
+  //   originRender(name, context, function(err, html) {
+  //     if (err) {
+  //       return callback(err);
+  //     }
+  //     return callback(html);
+  //   });
+  // };
+
+  engine.renderString = function(str, context, cb) {
+    str = originRenderString(str, context, function(err, html) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(html);
+    });
+  };
+}
+
 module.exports = app => {
   app.get('/', function* () {
     yield this.render('home.tpl', { user: 'egg' });
   });
 
+  const engine = app.nunjucks;
+
   app.get('/string', function* () {
-    this.body = yield this.renderString('hi, {{ user }}', { user: 'egg' });
+    hackRender(engine);
+    this.body = yield this.renderString('hi, {{ user }}', { user: 'egg' }, {
+      viewEngine: 'nunjucks',
+    });
   });
 
   app.get('/string_options', function* () {
